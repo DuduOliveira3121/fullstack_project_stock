14d81200e6774f20d6134fdd4a2d8f5982d382a2
DuduOliveira3121 <duduoliveiras3121@gmail.com>
2026-03-19 22:01:36 -0300

    Adding the message

--- src/Infrastructure/http/whats_app.py
+++ src/Infrastructure/http/whats_app.py
@@ -2,5 +2,5 @@ class Whatsapp:
     @staticmethod
     def send_message(phone, code):
         # Por enquanto, apenas retorna sucesso
-        # TODO: Implementar integração real com Twilio (Arthur!!!)
+        # TODO: Implementar integração real com Twilio (Arthur)
         return {"success": True, "message": f"Código {code} seria enviado para {phone}"}
