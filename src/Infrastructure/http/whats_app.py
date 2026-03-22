d62378e5c3145775fc3b869f95ede8ced099fd88
Arthur Goncalves rosa <Arthurgoncalvesrosa08@gmail.com>
2026-03-21 23:28:10 -0300

    feat: implementa integração real com Twilio e fluxo de ativação do Seller: sem teste REAL

--- src/Infrastructure/http/whats_app.py
+++ src/Infrastructure/http/whats_app.py
@@ -1,6 +1,29 @@
+import os
+from dotenv import load_dotenv
+from twilio.rest import Client
+
+load_dotenv() # Carrega o .env
+
 class Whatsapp:
+    ACCOUNT_SID = os.getenv("TWILIO_ACCOUNT_SID")
+    AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
+    FROM_WHATSAPP = "whatsapp:+14155238886"
+
     @staticmethod
     def send_message(phone, code):
-        # Por enquanto, apenas retorna sucesso
-        # TODO: Implementar integração real com Twilio (Arthur)
-        return {"success": True, "message": f"Código {code} seria enviado para {phone}"}
+        try:
+            client = Client(Whatsapp.ACCOUNT_SID, Whatsapp.AUTH_TOKEN)
+            
+            # Limpa o número e garante o prefixo correto
+            clean_phone = phone.strip().replace(" ", "")
+            to_number = f"whatsapp:{clean_phone}"
+            
+            message = client.messages.create(
+                from_=Whatsapp.FROM_WHATSAPP,
+                body=f"Seu código de ativação fullstack Project: {code}",
+                to=to_number
+            )
+            return {"success": True, "sid": message.sid}
+        except Exception as e:
+            # Lança a exceção para ser capturada pelo Service
+            raise Exception(str(e))
