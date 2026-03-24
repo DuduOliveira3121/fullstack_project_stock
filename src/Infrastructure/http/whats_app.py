70e2f8014d05f45f24e5adebd05e90cea3484810
Arthur Goncalves rosa <Arthurgoncalvesrosa08@gmail.com>
2026-03-23 21:01:09 -0300

    feat: implementa fluxo de ativacao via whatsapp sem secrets

--- src/Infrastructure/http/whats_app.py
+++ src/Infrastructure/http/whats_app.py
@@ -1,29 +1,22 @@
 import os
-from dotenv import load_dotenv
 from twilio.rest import Client
 
-load_dotenv() # Carrega o .env
-
 class Whatsapp:
+    # Pega das variáveis de ambiente do seu PC ou servidor
     ACCOUNT_SID = os.getenv("TWILIO_ACCOUNT_SID")
     AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
-    FROM_WHATSAPP = "whatsapp:+55155238886"
+    FROM_WHATSAPP = "whatsapp:+14155238886"
 
     @staticmethod
     def send_message(phone, code):
         try:
             client = Client(Whatsapp.ACCOUNT_SID, Whatsapp.AUTH_TOKEN)
-            
-            # Limpa o número e garante o prefixo correto
             clean_phone = phone.strip().replace(" ", "")
-            to_number = f"whatsapp:{clean_phone}"
-            
             message = client.messages.create(
                 from_=Whatsapp.FROM_WHATSAPP,
-                body=f"Seu código de ativação fullstack Project: {code}",
-                to=to_number
+                body=f"Seu codigo de ativacao Stock Project: {code}",
+                to=f"whatsapp:{clean_phone}"
             )
             return {"success": True, "sid": message.sid}
         except Exception as e:
-            # Lança a exceção para ser capturada pelo Service
-            raise Exception(str(e))
+            raise Exception(f"Erro Twilio: {str(e)}")
